1ead24b3812917db1e4300b04cfb9b34e69f32f6
vlapugb <vladimirpugovkinspb@gmail.com>
2024-10-09 22:31:48 +0300

    fix: tried second attempt some fixes

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
 use std::io;
 
-fn counting(vec: &Vec<char>, mut left: isize, mut right: isize) -> (isize, isize) {
-    while left >= 0 && right < vec.len() as isize && vec[left as usize] == vec[right as usize] {
+fn counting(vec: &Vec<char>, mut left: usize, mut right: usize) -> (usize, usize) {
+    while left > 0 && right < vec.len() as usize && vec[left as usize] == vec[right as usize] {
         left -= 1;
         right += 1;
     }
-    (left + 1, right)
+    (left, right)
 }
 
 fn longest_polindrom(input_str: &str) -> &str {
     let vec: Vec<_> = input_str.chars().collect();
-    let mut max_size_of_palindrome: (isize, isize) = (0, 0);
+    let mut max_size_of_palindrome: (usize, usize) = (0, 0);
 
     for i in 0..vec.len() - 1 {
-        let size_palindrome_odd = counting(&vec, i as isize, i as isize + 1);
-        let size_palindrome_even = counting(&vec, i as isize, i as isize);
-        let max_of_even_or_odd: (isize, isize);
+        let size_palindrome_odd = counting(&vec, i as usize, i as usize + 1);
+        let size_palindrome_even = counting(&vec, i as usize, i as usize);
+        let max_of_even_or_odd: (usize, usize);
         if size_palindrome_even.1 - size_palindrome_even.0
             > size_palindrome_odd.1 - size_palindrome_odd.0
         {
@@ -31,7 +31,7 @@ fn longest_polindrom(input_str: &str) -> &str {
         }
     }
 
-    &input_str[(max_size_of_palindrome.0 as usize)..(max_size_of_palindrome.1 as usize)]
+    &input_str[(max_size_of_palindrome.0 as usize)..=(max_size_of_palindrome.1 as usize)]
 }
 
 fn main() {
